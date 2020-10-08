d29bba27b252bf4eb213a430c77e38e7abe165ac
deeborges <deyvisonsilvaborges@gmail.com>
2020-10-08 10:41:33 -0300

    fix(simulador): estrutura inical finalizada

---
+++ src/snack.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ExpoSnack from 'react-expo-snack';
+
+export default function Snack() {
+  return (
+    /* 
+      no seu navegador, ao acessar um snack dentro do expo.io, copie o seu @userame/nome-projeto 
+      e cole dentro do "id" logo abaixo.
+    */
+    <ExpoSnack id="@deyv/excited-sandwich" />
+  );
+}
